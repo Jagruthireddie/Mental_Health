ab90321f91543f83d71be9efbcf4bf3c01800260
Jagruthireddie <161810025+Jagruthireddie@users.noreply.github.com>
2026-05-27 14:15:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
 import pickle
 import joblib
 import matplotlib.pyplot as plt
-
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Load model
+model = load_model("mental_health_rnn_model.h5")
 
 # ============================================
 # PAGE CONFIG
